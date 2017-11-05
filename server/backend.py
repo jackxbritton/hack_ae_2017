6fd6e90597653a9d7b280b08195481abce543b38
Jack Britton <jackxbritton@gmail.com>
2017-11-04 22:07:01 -0400

    Endpoints for sending GPS and battery data to.

---
+++ server/backend.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import psycopg2
+from uuid import UUID
+
+def uuid_to_id(cursor, uuid):
+    # Get user id.
+    cursor.execute('SELECT id FROM users WHERE users.uuid = %s', (uuid,))
+    id = cursor.fetchone()
+    # Insert new user if necessary.
+    if id is None:
+        cursor.execute('INSERT INTO users (uuid) VALUES (%s)', (uuid,))
+        cursor.execute('SELECT id FROM users WHERE users.uuid = %s', (uuid,))
+        id = cursor.fetchone()
+    return id[0]
+
+def insert_battery(db_str, uuid, timestamp, battery):
+    # Parse inputs.
+    try:
+        uuid = str(UUID(uuid))
+        timestamp = int(timestamp)
+        battery = float(battery)
+    except:
+        print('Couldn\'t parse inputs in insert_battery()')
+        return
+    # Connect to the database.
+    try:
+        db = psycopg2.connect(db_str)
+    except Exception as e:
+        print('Error: ' + str(e))
+        return
+    cursor = db.cursor()
+    # Get id.
+    id = uuid_to_id(cursor, uuid)
+    # Insert battery row.
+    cursor.execute(
+        'INSERT INTO battery (level, timestamp, user_id) VALUES (%s, %s, %s)',
+        (battery, timestamp, id)
+    )
+    db.commit()
+    # Clean up.
+    cursor.close()
+    db.close()
+
+def insert_gps(db_str, uuid, timestamp, lat, lon):
+    # Parse inputs.
+    try:
+        uuid = str(UUID(uuid))
+        timestamp = int(timestamp)
+        lat = float(lat)
+        lon = float(lon)
+    except:
+        print('Couldn\'t parse inputs in insert_gps()')
+        return
+    # Connect to the database.
+    try:
+        db = psycopg2.connect(db_str)
+    except Exception as e:
+        print('Error: ' + str(e))
+    cursor = db.cursor()
+
+    # Parse inputs.
+    try:
+        uuid = str(UUID(uuid))
+        timestamp = int(timestamp)
+        lat = float(lat)
+        lon = float(lon)
+    except:
+        print('Couldn\'t parse inputs in insert_gps()')
+        return
+    # Connect to the database.
+    try:
+        db = psycopg2.connect(db_str)
+    except Exception as e:
+        print('Error: ' + str(e))
+        return
+    cursor = db.cursor()
+    # Get id.
+    id = uuid_to_id(cursor, uuid)
+    # Insert GPS row.
+    cursor.execute(
+        'INSERT INTO gps (lat, lon, timestamp, user_id) VALUES (%s, %s, %s, %s)',
+        (lat, lon, timestamp, id)
+    )
+    db.commit()
+    # Clean up.
+    cursor.close()
+    db.close()
--- server/main.py
+++ server/main.py
@@ -1,33 +1,57 @@
 #!/usr/bin/env python
 
 import flask
-import psycopg2
 import sys
 from getpass import getpass
 
-# CLI args.
-if len(sys.argv) < 4:
-    print('Usage: ' + sys.argv[0] + ' PSQL_USER PSQL_HOST DB')
-    sys.exit(1)
+import backend
 
-# Put together the database connection string. Note the getpass()!
-db_str = 'user=\'' + sys.argv[1] + '\' host=\'' + sys.argv[2] + '\' dbname=\'' + sys.argv[3] + '\' password=\'' + getpass() + '\''
-
-# Flask stuff.
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def test():
-    # Connect to the database.
-    try:
-        db = psycopg2.connect(db_str)
-    except Exception as e:
-        print('Error: ' + str(e))
-    # Serve JSON.
-    if 'x' in flask.request.args:
-        return str(flask.request.args['x'])
-    else:
-        return 'no parameter \'x\''
+@app.route('/bat', methods=['GET','POST'])
+def bat():
+    # Check necessary parameters.
+    if 'uuid' not in flask.request.args:
+        return '' # TODO Actual error code.
+    if 'ts' not in flask.request.args:
+        return '' # TODO Actual error code.
+    if 'bat' not in flask.request.args:
+        return '' # TODO Actual error code.
+    backend.insert_battery(
+        db_str,
+        flask.request.args['uuid'],
+        flask.request.args['ts'],
+        flask.request.args['bat']
+    )
+    return 'Good.' # TODO Actual response.
+
+@app.route('/gps', methods=['GET','POST'])
+def gps():
+    # Check necessary parameters.
+    if 'uuid' not in flask.request.args:
+        return '' # TODO Actual error code.
+    if 'ts' not in flask.request.args:
+        return '' # TODO Actual error code.
+    if 'lat' not in flask.request.args:
+        return '' # TODO Actual error code.
+    if 'lon' not in flask.request.args:
+        return '' # TODO Actual error code.
+    # GPS.
+    backend.insert_gps(
+        db_str,
+        flask.request.args['uuid'],
+        flask.request.args['ts'],
+        flask.request.args['lat'],
+        flask.request.args['lon']
+    )
+    return 'Good.' # TODO Actual response.
 
 if __name__ == '__main__':
+    # CLI args.
+    if len(sys.argv) < 4:
+        print('Usage: ' + sys.argv[0] + ' PSQL_USER PSQL_HOST DB')
+        sys.exit(1)
+    # Put together the database connection string. Note the getpass()!
+    db_str = 'user=\'' + sys.argv[1] + '\' host=\'' + sys.argv[2] + '\' dbname=\'' + sys.argv[3] + '\' password=\'' + getpass() + '\''
+    # Run flask.
     app.run()
